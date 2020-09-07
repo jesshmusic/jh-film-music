8557aea039f7ef811206272f0f3f65addf21b4a5
jesshmusic <jhendricks@existentialmusic.com>
2020-09-07 09:29:45 -0400

    Set up the video players

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
+import "../node_modules/video-react/dist/video-react.css";
 import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
 import About from "./pages/About";
 import Footer from "./components/Footer";
 
-export default function App() {
+const App = ({ store }) => {
   return (
+    <Provider store={store}>
       <Router>
         <NavBar />
         <Routes />
         <Footer />
       </Router>
+    </Provider>
   );
 }
 
@@ -32,3 +36,9 @@ const Routes = () => {
     </Switch>
   );
 }
+
+App.propTypes = {
+  store: PropTypes.object.isRequired
+}
+
+export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Container from "../components/Container";
 import jessPortrait from '../assets/images/JHendricksPortrait.jpg';
+import { Player } from 'video-react';
+import DemoReel1 from '../assets/videos/JHDemoReel1.mp4';
+import styles from './Home.module.scss';
 
 const Home = () => {
   return (
     <Container>
-      <div>
+      <div className={styles.home}>
         <h1>Jess Hendricks <small>Composer for film, games, and media</small></h1>
-        <img src={jessPortrait} />
+        <Player
+          playsInline
+          src={DemoReel1}
+        />
+        <img src={jessPortrait} alt={'Jess Hendricks'} />
       </div>
     </Container>
   );
