4054a652b1862f3cf3d306f8abee0580b10e938f
jesshmusic <jhendricks@existentialmusic.com>
2020-09-08 13:38:17 -0400

    Pull in posts for Nav as well

--- src/App.js
+++ src/App.js
@@ -5,27 +5,18 @@ import {
   Route,
 } from "react-router-dom";
 
-import Pages from "./data-pages";
 import Page from "./pages/Page";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import ContactForm from "./components/Contact";
+import BackgroundImage from './assets/images/topography.svg';
+
+import styles from './App.module.scss';
+import PropTypes from "prop-types";
 
 const restApiUrl = process.env.NODE_ENV === 'development' ? 'https://existentialmusic.local.com/wp-json/wp/v2/film_music_page' : 'https://existentialmusic.com/wp-json/wp/v2/film_music_page'
 
 const App = () => {
-
-  return (
-    <Router>
-      <NavBar />
-      <Routes />
-      <ContactForm />
-      <Footer />
-    </Router>
-  );
-}
-
-const Routes = () => {
   const [ posts, setPosts ] = useState([]);
   useEffect(() => {
     async function loadPosts() {
@@ -42,6 +33,22 @@ const Routes = () => {
     loadPosts();
   }, []);
   return (
+    <Router>
+      <div className={styles.app}>
+        <div style={{backgroundImage: `url(${BackgroundImage})`}} className={styles.appWrapper}>
+          <NavBar posts={posts} />
+          <Routes posts={posts} />
+          <ContactForm />
+          <Footer />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+const Routes = ({posts}) => {
+
+  return (
     <Switch>
       {posts.map((post, index) => (
         <Route path={post.relativeRoute} key={index}>
@@ -50,6 +57,10 @@ const Routes = () => {
       ))}
     </Switch>
   );
+}
+
+Routes.propTypes = {
+  posts: PropTypes.array,
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,23 @@
 import styles from "../styles/components/NavBar.module.scss";
 import {Link} from "react-router-dom";
 import React from "react";
+import PropTypes from "prop-types";
 
-const NavBar = () => {
+const NavBar = ({posts}) => {
+  const sortedPosts = posts.sort((a, b) => a.id > b.id ? 1: -1);
   return (
     <div className={styles.navbarContainer}>
       <nav className={styles.navbarMain}>
-        <Link to="/" className='nav-link'>Home</Link>
-        <Link to="/about" className="nav-link">About</Link>
+        {sortedPosts.map((post, index) => (
+          post.showInNav ? <Link to={post.relativeRoute} className={styles.navLink} key={index}>{post.navTitle}</Link> : null
+        ))}
       </nav>
     </div>
   );
+}
+
+NavBar.propTypes = {
+  posts: PropTypes.array,
 }
 
 export default NavBar;
